022382ecfbc460e53aaec2c9e6521a35838e4f14
coder-1990 <asrorakmalovich@gmail.com>
2021-12-23 15:48:49 +0500

    init commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//Elementlarni tanlab olinadi
+const elCurrencyConverterForm = document.querySelector('.js-currency-converter-form');
+const elAmountInput = elCurrencyConverterForm.querySelector('.js-currency-amount-input');
+const elCurrencySelect = elCurrencyConverterForm.querySelector('.js-currency-select');
+const elResultDisplay = elCurrencyConverterForm.querySelector('.result-display');
+
+//Resultatga chiqarish uchun funksiya
+const displayResult = function () {
+  const result = Number(elAmountInput.value) * Number(elCurrencySelect.value) / 100;
+  elResultDisplay.textContent = result.toFixed(2);
+};
+
+//Submit bo'lganda tabiiy hodisani oldini olamiz
+elCurrencyConverterForm.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  //Resultatni korsatish u-n funksiyani chaqirib qo'yamiz
+  displayResult();
+});
+
+//Resultatni usha zahoti(online) ko'rsatish uchun
+elAmountInput.addEventListener('keyup', function () {
+  displayResult();
+});
+
+//Selectni almashtirganda online o'zgarishi uchun
+elCurrencySelect.addEventListener('change', function () {
+  displayResult();
+});
